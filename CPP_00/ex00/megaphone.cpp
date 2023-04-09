127d7ef60e2c747542e3939d9ce6655445a6cc13
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-04-09 08:32:44 +0400

    ex 00 done

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 08:26:12 by cegbulef          #+#    #+#             */
+/*   Updated: 2023/04/09 08:26:14 by cegbulef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <iostream>
+
+std::string strUpper(std::string str) {
+	for (std::string::size_type i = 0; i < str.length(); i++)
+		str[i] = toupper(str[i]);
+	return (str);
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+    else {
+        for (int i = 1; i < argc; i++)
+            std::cout << strUpper(argv[i]);
+        std::cout << std::endl;
+    }
+	return (0);
+}
