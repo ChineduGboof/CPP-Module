509728d46f635e98f95a377dba0c6b472697ddf6
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-04-11 17:46:44 +0400

    CPP01 ex05 done

--- CPP_01/ex05/Harl.hpp
+++ CPP_01/ex05/Harl.hpp
@@ -6,7 +6,43 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:06:56 by cegbulef          #+#    #+#             */
-/*   Updated: 2023/04/11 15:06:58 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:41:32 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <map>
+#include <functional>
+#include <string>
+
+#define DEFAULT	"\033[0m"
+#define RED 	"\e[0;31m"
+#define GREEN 	"\e[0;32m"
+#define YELLOW 	"\e[0;33m"
+#define BLUE 	"\e[0;34m"
+#define MAGENTA	"\e[0;35m"
+#define CYAN	"\e[0;36m"
+#define WHITE	"\e[0;37m"
+
+class Harl {
+private:
+    typedef void (Harl::*FuncPtr)(void);
+    std::map<std::string, FuncPtr> levels;
+
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+
+public:
+    Harl();
+    void complain(std::string level);
+};
+
+#endif
+
+
 
--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -6,7 +6,21 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:07:00 by cegbulef          #+#    #+#             */
-/*   Updated: 2023/04/11 15:07:01 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:45:18 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    std::cout << YELLOW;
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    std::cout << RED;
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    std::cout << DEFAULT;
+    return 0;
+}
