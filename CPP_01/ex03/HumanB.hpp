d59e4f0999615ba9a3078bed74debe2e652c384f
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-04-16 18:53:38 +0400

    done

--- CPP_01/ex03/HumanB.hpp
+++ CPP_01/ex03/HumanB.hpp
@@ -6,7 +6,7 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:28:27 by cegbulef          #+#    #+#             */
-/*   Updated: 2023/04/11 12:28:43 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/16 16:56:07 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@
 #include <iostream>
 
 class HumanB {
+    private:
+        std::string name;
+        const Weapon *weapon;
     public:
         HumanB(const std::string &name);
         void setWeapon(const Weapon &weapon);
         void attack() const;
-
-    private:
-        std::string name;
-        const Weapon *weapon;
 };
 
 #endif
