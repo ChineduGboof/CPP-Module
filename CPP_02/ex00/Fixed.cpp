3c8d6b7db69fee457035d3bca967abe398403066
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-04-18 20:00:28 +0400

    ex00 done CPP02

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -6,7 +6,34 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 16:42:48 by cegbulef          #+#    #+#             */
-/*   Updated: 2023/04/18 16:42:50 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:59:17 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed():_fixedPnbr(0){
+    std::cout <<YELLOW "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & src){
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+    return;
+}
+
+int Fixed::getRawBits(void)const{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixedPnbr;
+}
+
+Fixed & Fixed::operator=(Fixed const & rhs){
+    std::cout << "Copy assignment operator called " << std::endl;
+    if (this != &rhs)
+        this->_fixedPnbr = rhs.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed(void){
+    std::cout << YELLOW "Destructor called" DEFAULT << std::endl;
+}
--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 16:42:51 by cegbulef          #+#    #+#             */
-/*   Updated: 2023/04/18 18:20:33 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:59:06 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,5 @@ class Fixed{
         ~Fixed(void);
         int getRawBits(void)const;
 };
-
-Fixed::Fixed():_fixedPnbr(0){
-    std::cout <<YELLOW "Default constructor called" << std::endl;
-}
-
-Fixed::Fixed(Fixed const & src){
-    std::cout << "Copy constructor called" << std::endl;
-    *this = src;
-    return;
-}
-
-int Fixed::getRawBits(void)const{
-    std::cout << "getRawBits member function called" << std::endl;
-    return this->_fixedPnbr;
-}
-
-Fixed & Fixed::operator=(Fixed const & rhs){
-    std::cout << "Copy assignment operator called " << std::endl;
-    if (this != &rhs)
-        this->_fixedPnbr = rhs.getRawBits();
-    return *this;
-}
-
-Fixed::~Fixed(void){
-    std::cout << YELLOW "Destructor called" DEFAULT << std::endl;
-}
 
 #endif
