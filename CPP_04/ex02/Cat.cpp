29d66e8e684db202d5867aec2847c90615daf17c
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-04-24 13:27:22 +0400

    adjustments made to 04

--- CPP_04/ex02/Cat.cpp
+++ CPP_04/ex02/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 20:54:26 by gboof             #+#    #+#             */
-/*   Updated: 2023/04/23 19:24:19 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/24 13:26:07 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,36 @@ Cat::Cat(): _brain(new Brain()) {
     std::cout << YELLOW << "A new Cat has been created" << DEFAULT << std::endl;
 }
 
+// Cat::Cat(const Cat & other): Animal(other) {
+//     _type = other._type;
+//     _brain = new Brain(*other._brain);
+//     std::cout << YELLOW << "A Cat has been copied" << DEFAULT << std::endl;
+// }
+
 Cat::Cat(const Cat & other): Animal(other) {
     _type = other._type;
-    _brain = new Brain(*other._brain);
+    if (other._brain) {
+        _brain = new Brain(*other._brain);
+    } else {
+        _brain = NULL;
+    }
     std::cout << YELLOW << "A Cat has been copied" << DEFAULT << std::endl;
 }
 
-Cat & Cat::operator=(const Cat & other){
+// Cat & Cat::operator=(const Cat & other){
+//     if (this != &other) {
+//         _type = other._type;
+//         _brain = new Brain(*other._brain);
+//     }
+//     std::cout << YELLOW << "A Cat has been assigned" << DEFAULT << std::endl;
+//     return *this;
+// }
+
+Cat& Cat::operator=(const Cat& other) {
     if (this != &other) {
+        delete _brain;
+        _brain = other._brain ? new Brain(*other._brain) : NULL;
         _type = other._type;
-        _brain = new Brain(*other._brain);
     }
     std::cout << YELLOW << "A Cat has been assigned" << DEFAULT << std::endl;
     return *this;
--- CPP_04/ex02/Dog.cpp
+++ CPP_04/ex02/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 20:54:31 by gboof             #+#    #+#             */
-/*   Updated: 2023/04/23 19:25:30 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/24 13:26:52 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,35 @@ Dog::Dog(): _brain(new Brain()) {
     std::cout << YELLOW << "A new Dog has been created" << DEFAULT << std::endl;
 }
 
+// Dog::Dog(const Dog & other): Animal(other) {
+//     _type = other._type;
+//     _brain = new Brain(*other._brain);
+//     std::cout << YELLOW << "A Dog has been copied" << DEFAULT << std::endl;
+// }
+
 Dog::Dog(const Dog & other): Animal(other) {
     _type = other._type;
-    _brain = new Brain(*other._brain);
+    if (other._brain) {
+        _brain = new Brain(*other._brain);
+    } else {
+        _brain = NULL;
+    }
     std::cout << YELLOW << "A Dog has been copied" << DEFAULT << std::endl;
 }
+// Dog & Dog::operator=(const Dog & other){
+//     if (this != &other) {
+//         _type = other._type;
+//         _brain = new Brain(*other._brain);
+//     }
+//     std::cout << YELLOW << "A Dog has been assigned" << DEFAULT << std::endl;
+//     return *this;
+// }
 
-Dog & Dog::operator=(const Dog & other){
+Dog& Dog::operator=(const Dog& other) {
     if (this != &other) {
+        delete _brain;
+        _brain = other._brain ? new Brain(*other._brain) : NULL;
         _type = other._type;
-        _brain = new Brain(*other._brain);
     }
     std::cout << YELLOW << "A Dog has been assigned" << DEFAULT << std::endl;
     return *this;
