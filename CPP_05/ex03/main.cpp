5091a9f0662e673fe77238feee6d03bbab8860f5
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-05-03 09:40:19 +0400

    added main

--- CPP_05/ex03/main.cpp
+++ CPP_05/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gboof <gboof@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 21:01:23 by gboof             #+#    #+#             */
-/*   Updated: 2023/05/03 09:36:07 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/05/03 09:40:05 by gboof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 #include "ShrubberyCreationForm.hpp"
 #include "Intern.hpp"
 
-int main()
+int main(void)
 {
-    {
-        Intern someRandomIntern;
-        AForm* rrf;
-        rrf = someRandomIntern.makeForm("Robotomy Request", "Bender");
-    }
+    Intern someRandomIntern;
+    AForm *rrf;
+    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+    Bureaucrat b = Bureaucrat("benjamin", 1);
+    b.signAForm(*rrf);
+    b.executeForm(*rrf);
+    delete rrf;
 }
